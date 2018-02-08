8cc8aeb7ecbee6dc8a1ab3b4d9113e3c71e31976
Cyril Puccio <cyril.puccio@epitech.eu>
2018-02-08 17:32:28 +0100

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/michel/team/epicture/Constants.kt
+++ app/src/main/java/com/michel/team/epicture/Constants.kt
@@ -1,11 +1,11 @@
-package com.michel.team.epicture;
+package com.michel.team.epicture
 
 /**
  * Created by puccio_c on 2/8/18.
  */
 
 object Constants {
-        val BASE_URL = "https://api.instagram.com/"
-        val CLIENT_ID = "241bbffd7e3a4de094ff6d0abf4ddadb"
-        val REDIRECT_URI = "https://instagram.com/"
-    }
+    val BASE_URL = "https://api.instagram.com/"
+    val CLIENT_ID = "d973bad8d6584184b7188474d8c8c3a4"
+    val REDIRECT_URI = "https://instagram.com/"
+}
