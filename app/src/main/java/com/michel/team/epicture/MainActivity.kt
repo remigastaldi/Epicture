c1c483969ce0ab5b770aa0d1c41530ef00c40b97
Unknown <remi.gastaldi@epitech.eu>
2018-02-08 17:31:39 +0100

    Add instagram api

--- app/src/main/java/com/michel/team/epicture/MainActivity.kt
+++ app/src/main/java/com/michel/team/epicture/MainActivity.kt
@@ -2,12 +2,25 @@ package com.michel.team.epicture
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.putraxor.instagram.Instagram
+import android.os.StrictMode
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+
+        StrictMode.setThreadPolicy(policy)
+        var ig = Instagram
+        ig.username = "pratulisian"
+        ig.password = "qwertypoi"
+        ig.prepare()
+        ig.login()
     }
 
 }
