8da43b462617e247704bfc2e954167e8fd68870e
Cyril Puccio <cyril.puccio@epitech.eu>
2018-02-08 15:41:02 +0100

    Merge

---
+++ app/src/main/java/com/michel/team/epicture/Constants.java
@@ -0,0 +1,11 @@
+package com.michel.team.epicture;
+
+/**
+ * Created by puccio_c on 2/8/18.
+ */
+
+object Constants {
+        val BASE_URL = "https://api.instagram.com/"
+        val CLIENT_ID = "241bbffd7e3a4de094ff6d0abf4ddadb"
+        val REDIRECT_URI = "https://instagram.com/"
+    }
