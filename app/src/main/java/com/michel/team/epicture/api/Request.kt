eead6097a2450b6b66258715f610a32af556c46a
Unknown <remi.gastaldi@epitech.eu>
2018-02-10 23:05:42 +0100

    Correct Request warnings

--- app/src/main/java/com/michel/team/epicture/api/Request.kt
+++ app/src/main/java/com/michel/team/epicture/api/Request.kt
@@ -6,10 +6,10 @@ import khttp.responses.Response
 import khttp.structures.cookie.CookieJar
 
 class Request {
-    var url = "";
-    var data = "";
+    var url = ""
+    var data = ""
     var isGet = true
-    var persistedCookies: CookieJar = CookieJar()
+    var persistedCookies: CookieJar? = null
     var headers = HTTP.HEADERS
 
     fun prepare(reqUrl: String, jsonString: String = ""): Request {
@@ -23,27 +23,27 @@ class Request {
 
     fun send(): Response {
         //println("Cookies for ${url} is ${persistedCookies?.size} -> ${persistedCookies?.entries}")
-        var resp: Response
+        val resp: Response
         if (isGet) {
-            if (persistedCookies == null) {
-                resp = get(url, headers = headers)
+            resp = if (persistedCookies == null) {
+                get(url, headers = headers)
             } else {
-                resp = get(url, headers = headers, cookies = persistedCookies)
+                get(url, headers = headers, cookies = persistedCookies)
             }
         } else {
-            val signature = data?.let { Crypto.signData(data) }
-            val payload = mapOf("signed_body" to "${signature?.signed}.${signature?.payload}", "ig_sig_key_version" to signature?.sigKeyVersion)
+            val signature = data.let { Crypto.signData(data) }
+            val payload = mapOf("signed_body" to "${signature.signed}.${signature.payload}", "ig_sig_key_version" to signature.sigKeyVersion)
             //println("Request.kt PayLoad = $payload")
-            if (persistedCookies == null) {
-                resp = post(url, headers = headers, data = payload)
+            resp = if (persistedCookies == null) {
+                post(url, headers = headers, data = payload)
             } else {
-                resp = post(url, headers = headers, data = payload, cookies = persistedCookies)
+                post(url, headers = headers, data = payload, cookies = persistedCookies)
             }
         }
         if (persistedCookies == null) {
             persistedCookies = resp.cookies
         } else {
-            persistedCookies.putAll(resp.cookies)
+            persistedCookies!!.putAll(resp.cookies)
         }
         return resp
     }
